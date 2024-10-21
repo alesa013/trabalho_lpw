77afd43d583fbf15d824e11ff0ea61ceae0ca2a8
Laécio <laeciojesusn@gmail.com>
2024-10-20 21:03:20 -0300

    Add pesquisa de CEP usando AJAX e melhorias no HTML

--- pesquisa-form/ajax.js
+++ pesquisa-form/ajax.js
@@ -1,35 +1,24 @@
-function consultaCep() {
-    let cep = document.getElementById("cep").value;
+let xhr = new XMLHttpRequest(); //criando um objeto
 
-//Verificar se o CEP for diferente, não é CEP válido.
-    if (cep.length !==8) {
-        alert("CEP inválido!");
-        return; //sai da função e não continua.
-    }
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
-
-  //Passa uma função e espera o parâmetro "response".
-    fetch(url).then(function(response){
-        //converte para objeto, e espera a função que vai receber os dados.
-        response.json().then(function(data) {
-            if (data.erro) {
-                alert("CEP não encontrado!");
+//inciando a função
+function consultaCep(cep){
+    //verifica se o cep tem 8 caracteres
+    if(cep.length == 8){
+        //readystate 4: requisição concluída e a resposta está pronta
+        //response 200: tudo ocorreu bem
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4 && xhr.status == 200){
+                //converte a resposta do servidor em objeto
+                let retorno = JSON.parse(xhr.responseText);
+                document.querySelector('#logradouro').value = retorno.logradouro;
+                document.querySelector('#complemento').value = retorno.complemento;
+                document.querySelector('#bairro').value = retorno.bairro;
+                document.querySelector('#cidade').value = retorno.localidade;
+                document.querySelector('#uf').value = retorno.uf;
             }
-            mostrarDados(data);
-        });
-    }).catch(function(erro){
-        console.log("Erro na requisição", erro);
-    });
+        }
+        //configura a requisição junto com o cep
+        xhr.open('GET', `https://viacep.com.br/ws/${cep}/json/`);
+        xhr.send(); //envia a requisição
+   }
 }
-
-function mostrarDados(dados){
-    document.getElementById("logradouro").value = dados.logradouro;
-    document.getElementById("complemento").value = dados.complemento;
-    document.getElementById("bairro").value = dados.bairro;
-    document.getElementById("cidade").value = dados.localidade;
-    document.getElementById("uf").value = dados.uf;
-}
-
-
-
-
